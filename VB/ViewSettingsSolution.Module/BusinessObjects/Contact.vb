249b54d3c6f3c17bda00416e8eab6932dbd70c99
DevExpressExampleBot <fakeEmail>
2020-09-15 11:04:19 +0300

    Source auto update [skip ci]

--- VB/ViewSettingsSolution.Module/BusinessObjects/Contact.vb
+++ VB/ViewSettingsSolution.Module/BusinessObjects/Contact.vb
@@ -7,59 +7,59 @@ Imports DevExpress.Xpo
 Imports DevExpress.Persistent.BaseImpl
 
 Namespace ViewSettingsSolution.Module.BusinessObjects
-    <DefaultClassOptions> _
-    Public Class Contact
-        Inherits BaseObject
+	<DefaultClassOptions>
+	Public Class Contact
+		Inherits BaseObject
 
-
-        Private webPageAddress_Renamed As String
-
-        Private nickName_Renamed As String
-
-        Private spouseName_Renamed As String
-
-        Private titleOfCourtesy_Renamed As TitleOfCourtesy
-        Public Sub New(ByVal session As Session)
-            MyBase.New(session)
-        End Sub
-        Public Property WebPageAddress() As String
-            Get
-                Return webPageAddress_Renamed
-            End Get
-            Set(ByVal value As String)
-                SetPropertyValue("WebPageAddress", webPageAddress_Renamed, value)
-            End Set
-        End Property
-        Public Property NickName() As String
-            Get
-                Return nickName_Renamed
-            End Get
-            Set(ByVal value As String)
-                SetPropertyValue("NickName", nickName_Renamed, value)
-            End Set
-        End Property
-        Public Property SpouseName() As String
-            Get
-                Return spouseName_Renamed
-            End Get
-            Set(ByVal value As String)
-                SetPropertyValue("SpouseName", spouseName_Renamed, value)
-            End Set
-        End Property
-        Public Property TitleOfCourtesy() As TitleOfCourtesy
-            Get
-                Return titleOfCourtesy_Renamed
-            End Get
-            Set(ByVal value As TitleOfCourtesy)
-                SetPropertyValue("TitleOfCourtesy", titleOfCourtesy_Renamed, value)
-            End Set
-        End Property
-    End Class
-    Public Enum TitleOfCourtesy
-        Dr
-        Miss
-        Mr
-        Mrs
-        Ms
-    End Enum
+'INSTANT VB NOTE: The field webPageAddress was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private webPageAddress_Conflict As String
+'INSTANT VB NOTE: The field nickName was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private nickName_Conflict As String
+'INSTANT VB NOTE: The field spouseName was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private spouseName_Conflict As String
+'INSTANT VB NOTE: The field titleOfCourtesy was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private titleOfCourtesy_Conflict As TitleOfCourtesy
+		Public Sub New(ByVal session As Session)
+			MyBase.New(session)
+		End Sub
+		Public Property WebPageAddress() As String
+			Get
+				Return webPageAddress_Conflict
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue("WebPageAddress", webPageAddress_Conflict, value)
+			End Set
+		End Property
+		Public Property NickName() As String
+			Get
+				Return nickName_Conflict
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue("NickName", nickName_Conflict, value)
+			End Set
+		End Property
+		Public Property SpouseName() As String
+			Get
+				Return spouseName_Conflict
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue("SpouseName", spouseName_Conflict, value)
+			End Set
+		End Property
+		Public Property TitleOfCourtesy() As TitleOfCourtesy
+			Get
+				Return titleOfCourtesy_Conflict
+			End Get
+			Set(ByVal value As TitleOfCourtesy)
+				SetPropertyValue("TitleOfCourtesy", titleOfCourtesy_Conflict, value)
+			End Set
+		End Property
+	End Class
+	Public Enum TitleOfCourtesy
+		Dr
+		Miss
+		Mr
+		Mrs
+		Ms
+	End Enum
 End Namespace
--- VB/ViewSettingsSolution.Module/BusinessObjects/SettingsStore.vb
+++ VB/ViewSettingsSolution.Module/BusinessObjects/SettingsStore.vb
@@ -8,77 +8,78 @@ Imports System.ComponentModel
 Imports DevExpress.Xpo
 
 Namespace ViewSettingsSolution.Module.BusinessObjects
-    Public Class SettingsStore
-        Inherits BaseObject
-        Implements IObjectSpaceLink
+	Public Class SettingsStore
+		Inherits BaseObject
+		Implements IObjectSpaceLink
 
+'INSTANT VB NOTE: The field xml was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private xml_Conflict As String
+'INSTANT VB NOTE: The field name was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private name_Conflict As String
+'INSTANT VB NOTE: The field ownerId was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private ownerId_Conflict As String
+'INSTANT VB NOTE: The field viewId was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private viewId_Conflict As String
+'INSTANT VB NOTE: The field isShared was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private isShared_Conflict As Boolean
+'INSTANT VB NOTE: The field objectSpace was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private objectSpace_Conflict As IObjectSpace
+		Public Sub New(ByVal session As Session)
+			MyBase.New(session)
+		End Sub
+		<Browsable(False)>
+		<Size(SizeAttribute.Unlimited)>
+		Public Property Xml() As String
+			Get
+				Return xml_Conflict
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue("XML", xml_Conflict, value)
+			End Set
+		End Property
+		Public Property Name() As String
+			Get
+				Return name_Conflict
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue("Name", name_Conflict, value)
+			End Set
+		End Property
+		<Browsable(False)>
+		Public Property OwnerId() As String
+			Get
+				Return ownerId_Conflict
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue(Of String)("OwnerId", ownerId_Conflict, value)
+			End Set
+		End Property
+		Public Property IsShared() As Boolean
+			Get
+				Return isShared_Conflict
+			End Get
+			Set(ByVal value As Boolean)
+				SetPropertyValue("IsShared", isShared_Conflict, value)
+			End Set
+		End Property
+		<Browsable(False)>
+		Public Property ViewId() As String
+			Get
+				Return viewId_Conflict
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue(Of String)("ViewId", viewId_Conflict, value)
+			End Set
+		End Property
 
-        Private xml_Renamed As String
+		Private Property IObjectSpaceLink_ObjectSpace() As IObjectSpace Implements IObjectSpaceLink.ObjectSpace
+			Get
+				Return objectSpace_Conflict
+			End Get
 
-        Private name_Renamed As String
-
-        Private ownerId_Renamed As String
-
-        Private viewId_Renamed As String
-
-        Private isShared_Renamed As Boolean
-
-        Private objectSpace_Renamed As IObjectSpace
-        Public Sub New(ByVal session As Session)
-            MyBase.New(session)
-        End Sub
-        <Browsable(False), Size(SizeAttribute.Unlimited)> _
-        Public Property Xml() As String
-            Get
-                Return xml_Renamed
-            End Get
-            Set(ByVal value As String)
-                SetPropertyValue("XML", xml_Renamed, value)
-            End Set
-        End Property
-        Public Property Name() As String
-            Get
-                Return name_Renamed
-            End Get
-            Set(ByVal value As String)
-                SetPropertyValue("Name", name_Renamed, value)
-            End Set
-        End Property
-        <Browsable(False)> _
-        Public Property OwnerId() As String
-            Get
-                Return ownerId_Renamed
-            End Get
-            Set(ByVal value As String)
-                SetPropertyValue(Of String)("OwnerId", ownerId_Renamed, value)
-            End Set
-        End Property
-        Public Property IsShared() As Boolean
-            Get
-                Return isShared_Renamed
-            End Get
-            Set(ByVal value As Boolean)
-                SetPropertyValue("IsShared", isShared_Renamed, value)
-            End Set
-        End Property
-        <Browsable(False)> _
-        Public Property ViewId() As String
-            Get
-                Return viewId_Renamed
-            End Get
-            Set(ByVal value As String)
-                SetPropertyValue(Of String)("ViewId", viewId_Renamed, value)
-            End Set
-        End Property
-
-        Private Property IObjectSpaceLink_ObjectSpace() As IObjectSpace Implements IObjectSpaceLink.ObjectSpace
-            Get
-                Return objectSpace_Renamed
-            End Get
-
-            Set(ByVal value As IObjectSpace)
-                objectSpace_Renamed = value
-            End Set
-        End Property
-    End Class
+			Set(ByVal value As IObjectSpace)
+				objectSpace_Conflict = value
+			End Set
+		End Property
+	End Class
 End Namespace
