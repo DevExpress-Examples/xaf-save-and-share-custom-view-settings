cb44115162307e34d8b87e63f8180ffd1d158c1e
andrey <andrey.kozhevnikov@devexpress.com>
2023-03-15 17:15:21 +0400

    add xpo

---
+++ CS/XPO/ViewSettings/ViewSettings.Blazor.Server/Startup.cs
@@ -0,0 +1,102 @@
+﻿using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor.Services;
+using DevExpress.Persistent.Base;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Server.Circuits;
+using DevExpress.ExpressApp.Xpo;
+using ViewSettings.Blazor.Server.Services;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+using DevExpress.ExpressApp.Core;
+
+namespace ViewSettings.Blazor.Server;
+
+public class Startup {
+    public Startup(IConfiguration configuration) {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services) {
+        services.AddSingleton(typeof(Microsoft.AspNetCore.SignalR.HubConnectionHandler<>), typeof(ProxyHubConnectionHandler<>));
+
+        services.AddRazorPages();
+        services.AddServerSideBlazor();
+        services.AddHttpContextAccessor();
+        services.AddScoped<CircuitHandler, CircuitHandlerProxy>();
+        services.AddXaf(Configuration, builder => {
+            builder.UseApplication<ViewSettingsBlazorApplication>();
+            builder.Modules
+                .AddConditionalAppearance()
+                .AddValidation(options => {
+                    options.AllowValidationDetailsAccess = false;
+                })
+                .AddViewVariants()
+                .Add<ViewSettings.Module.ViewSettingsModule>()
+            	.Add<ViewSettingsBlazorModule>();
+            builder.ObjectSpaceProviders
+                .AddSecuredXpo((serviceProvider, options) => {
+                    string connectionString = null;
+                    if(Configuration.GetConnectionString("ConnectionString") != null) {
+                        connectionString = Configuration.GetConnectionString("ConnectionString");
+                    }
+#if EASYTEST
+                    if(Configuration.GetConnectionString("EasyTestConnectionString") != null) {
+                        connectionString = Configuration.GetConnectionString("EasyTestConnectionString");
+                    }
+#endif
+                    ArgumentNullException.ThrowIfNull(connectionString);
+                    options.ConnectionString = connectionString;
+                    options.ThreadSafe = true;
+                    options.UseSharedDataStoreProvider = true;
+                })
+                .AddNonPersistent();
+            builder.Security
+                .UseIntegratedMode(options => {
+                    options.RoleType = typeof(PermissionPolicyRole);
+                    // ApplicationUser descends from PermissionPolicyUser and supports the OAuth authentication. For more information, refer to the following topic: https://docs.devexpress.com/eXpressAppFramework/402197
+                    // If your application uses PermissionPolicyUser or a custom user type, set the UserType property as follows:
+                    options.UserType = typeof(ViewSettings.Module.BusinessObjects.ApplicationUser);
+                    // ApplicationUserLoginInfo is only necessary for applications that use the ApplicationUser user type.
+                    // If you use PermissionPolicyUser or a custom user type, comment out the following line:
+                    options.UserLoginInfoType = typeof(ViewSettings.Module.BusinessObjects.ApplicationUserLoginInfo);
+                    options.UseXpoPermissionsCaching();
+                })
+                .AddPasswordAuthentication(options => {
+                    options.IsSupportChangePassword = true;
+                });
+        });
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => {
+            options.LoginPath = "/LoginPage";
+        });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        if(env.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+        }
+        else {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. To change this for production scenarios, see: https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseRequestLocalization();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseXaf();
+        app.UseEndpoints(endpoints => {
+            endpoints.MapXafEndpoints();
+            endpoints.MapBlazorHub();
+            endpoints.MapFallbackToPage("/_Host");
+            endpoints.MapControllers();
+        });
+    }
+}
---
+++ CS/XPO/ViewSettings/ViewSettings.Module/Module.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Model.Core;
+using DevExpress.ExpressApp.Model.DomainLogics;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp.Xpo;
+using dxTestSolution.Module.DatabaseUpdate;
+
+namespace ViewSettings.Module;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.ModuleBase.
+public sealed class ViewSettingsModule : ModuleBase {
+    public ViewSettingsModule() {
+		// 
+		// ViewSettingsModule
+		// 
+		AdditionalExportedTypes.Add(typeof(DevExpress.Persistent.BaseImpl.ModelDifference));
+		AdditionalExportedTypes.Add(typeof(DevExpress.Persistent.BaseImpl.ModelDifferenceAspect));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.SystemModule.SystemModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Security.SecurityModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Validation.ValidationModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule));
+    }
+    public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
+        ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
+        return new ModuleUpdater[] { updater, new MyUpdater(objectSpace, versionFromDB) };
+    }
+    public override void Setup(XafApplication application) {
+        base.Setup(application);
+        // Manage various aspects of the application UI and behavior at the module level.
+    }
+    public override void CustomizeTypesInfo(ITypesInfo typesInfo) {
+        base.CustomizeTypesInfo(typesInfo);
+        CalculatedPersistentAliasHelper.CustomizeTypesInfo(typesInfo);
+    }
+}
